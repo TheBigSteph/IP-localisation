b28c976fbab9bf9589640301b81a31025ccd72a1
TheBigSteph <nzali.stephane86@gmail.com>
2023-02-14 15:11:55 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
+import styles from "./../src/app/page.module.css";
 import axios from "axios";
 import { useState } from "react";
 
@@ -72,56 +72,33 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.service}>
           <div>
-            <h3>Service 1: IPWHO </h3>
-            <button onClick={() => service('http://ipwho.is/')}>Valider</button>
+            <h3>Service 1: ABSTRACTAPI </h3>
+            <button onClick={() => service('https://ipgeolocation.abstractapi.com/v1/?api_key=0189f40f0ee74960b79fc02274fd632a')}>Valider</button>
           </div>
           <div>
             <h3>Service 2: IP GEOLOCALISATION</h3>
             <button onClick={() => service('https://api.ipgeolocation.io/ipgeo?apiKey=0b220fc73ca94e4b8eb795e6b4994241')}>Valider</button>
           </div>
           <div>
-            <h3>Service 3: IP API</h3>
-            <button onClick={() => service('http://ip-api.com/json/')}>Valider</button>
+            <h3>Service 3: IP2LOCATION</h3>
+            <button onClick={() => service('https://api.ip2location.com/v2/?key=J9Y4W4QW6E&format=json&package=WS25&&addon=continent,country,region,city,geotargeting')}>Valider</button>
           </div>
           <div>
-            <h3>Service 4: IPSTACK</h3>
-            <button onClick={() => service('http://api.ipstack.com/check?access_key=475174b90b148bfea3f29ac9e44ab2b4')}>Valider</button>
-          </div>
-          <div>
-            <h3>Service 5: IPAPI </h3>
+            <h3>Service 4: IPAPI </h3>
             <button onClick={() => service('https://ipapi.co/json/')}>Valider</button>
           </div>
           <div>
-            <h3>Service 6: IPIFY </h3>
+            <h3>Service 5: IPIFY </h3>
             <button onClick={() => service('https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_JOGj9MWCZJMtqgwOXbxa8sEqTO4nE')}>Valider</button>
           </div>
-          {/* <div>
-            <h3>Service 6: ipwho API</h3>
-            <button onClick={service1}>Valider</button>
-          </div>
-          <div>
-            <h3>Service 7: ipwho API</h3>
-            <button onClick={service1}>Valider</button>
-          </div>
-          <div>
-            <h3>Service 8: ipwho API</h3>
-            <button onClick={service1}>Valider</button>
-          </div>
-          <div>
-            <h3>Service 9: ipwho API</h3>
-            <button onClick={service1}>Valider</button>
-          </div>
-          <div>
-            <h3>Service 10: ipwho API</h3>
-            <button onClick={service1}>Valider</button>
-          </div> */}
+          
         </div>
 
         <div className="result">
           <ul>
-            <li>Ville: {result?.city || result?.location.city}</li>
-            <li>IP: {result?.ip || result?.query}</li>
-            <li>Code postal: {result?.postal || result?.zipcode || result?.zip || result?.location.postalCode} </li>
+            <li>Ville: {result?.city || result?.location.city || result?.city_name}</li>
+            <li>IP: {result?.ip || result?.query || result?.ip_address}</li>
+            <li>Code postal: {result?.postal || result?.zipcode || result?.zip_code || result?.zip || result?.location.postalCode || result?.postal_code} </li>
           </ul>
         </div>
       </main>
